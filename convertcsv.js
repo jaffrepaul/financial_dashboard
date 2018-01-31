9c63a12fd08d4c2727581d6f08c55dab1fc63b58
paulj <jaffrepaul@gmail.com>
2018-01-31 15:58:00 -0500

    added hot reloading, split data for efficiency & converted to JSON

---
+++ convertcsv.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, './src/data/csv/LoanStats3a_half2.csv');
+
+// Read CSV
+let f = fs.readFileSync(filePath, { encoding: 'utf-8' }, err => {
+  console.log(err);
+});
+
+// Split on row
+f = f.split('\n');
+
+// Get first row for column headers
+const headers = f.shift().split(',');
+
+const json = [];
+f.forEach(d => {
+  // Loop through each row
+  const tmp = {};
+  const row = d.split(',');
+  for (let i = 0; i < headers.length; i++) {
+    tmp[headers[i]] = row[i];
+  }
+  // Add object to collection
+  json.push(tmp);
+
+  // return JSON.stringify(json);
+});
+
+const outPath = path.join(__dirname, './src/data/data_half2.json');
+// Convert object to string, write json to file
+fs.writeFileSync(outPath, JSON.stringify(json), 'utf8', err => {
+  console.log(err);
+});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Bar, Line, Pie, Doughnut, Radar, Polar } from 'react-chartjs-2';
 
 import './App.css';
+import data1 from './data/data_half1.json';
+import data2 from './data/data_half2.json';
+// import preload from './data/data_half2.json';
 
 class App extends Component {
   render() {
     return (
+      // <div>
+      //   <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+      // </div>
       <div className="grid">
         <div className="one">one</div>
         <div className="two">two</div>
