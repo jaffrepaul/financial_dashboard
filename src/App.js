9770416e11e642a79170f4b20ad873296006ceef
paulj <jaffrepaul@gmail.com>
2018-01-31 16:32:35 -0500

    added linting, cleaned up code & project folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+// import LoanData from './LoanData';
 import data1 from './data/data_half1.json';
 import data2 from './data/data_half2.json';
 // import preload from './data/data_half2.json';
@@ -12,7 +13,7 @@ class App extends Component {
       //   <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
       // </div>
       <div className="grid">
-        <div className="one">one</div>
+        <div className="one" />
         <div className="two">two</div>
         <div className="three">three</div>
         <div className="four">four</div>
---
+++ src/LoanData.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { Bar, Line, Pie, Doughnut, Radar, Polar } from 'react-chartjs-2';
+
+/*
+pass in json data
+configure chartjs display options
+return chart with data & option props
+*/
